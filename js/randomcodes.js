306a5a29a699487b393154912f2487f8104e54de
Sophia Tapia <sophiatapia@MacBook-Pro-2.attlocal.net>
2022-03-02 00:53:34 -0800

    added function to generate code

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,8 +1,16 @@
-function verificationRecall() {
-    var verificationRecall = document.getElementById("verification-input").value;
-    document.getElementById("recall").innerHTML = verificationRecall;
+function generateCode() {
+    var code = ' ';
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+    for (i = 1; i <= 8; i++) {
+        var char = Math.random()*str.length;
+        code += str.charAt(char)
+    }
+    return code;
 }
-var display = document.getElementById("enter");
-display.addEventListener("click", function() {
-verificationRecall();
-});
+document.getElementById("codes").innerHTML = generateCode();
+
+function disableButton() {
+    document.getElementById("submit").disabled = true;
+}
+
+disableButton();
